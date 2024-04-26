93295edc9ac95b1a855aafa3dc5b11e115b4f465
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-04-25 23:31:20 -0700

    Everything working, just need to clean it up a bit and customize some more dark mode features

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,26 @@
 // Access toggle switch HTML element
 const themeSwitcher = document.querySelector('#theme-switcher');
 const body = document.querySelector('body');
+const headers = document.querySelector('h1');
+const link = document.getElementById('email');
 
 // Set default mode to dark
-let mode = 'dark';
+let mode = 'dark-mode';
 
 // Listen for a click event on toggle switch
 themeSwitcher.addEventListener('click', function () {
   // If mode is dark, apply light background
-  if (mode === 'dark') {
+  if (mode === 'dark-mode') {
     mode = 'light';
-    container.setAttribute('class', 'light');
+    body.setAttribute('class', 'light');
+    headers.setAttribute('class', 'light-header');
+    link.setAttribute('class', 'link');
   }
   // If mode is light, apply dark background
   else {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
+    mode = 'dark-mode';
+    body.setAttribute('class', 'dark-mode');
+    headers.setAttribute('class', 'dark-header');
+    link.setAttribute('class', 'dark-link');
   }
 });
